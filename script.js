0c0923d8bc4810a6e43e36d02a75dc0b4f6303ad
Ene-Daniele <ene.daniele@iisgalanimi.edu.it>
2021-11-11 18:37:21 +0100

    working spiral

--- script.js
+++ script.js
@@ -7,6 +7,32 @@ function domloaded() {
   context = document.getElementById("canvas").getContext("2d");
   context.fillStyle = "#ff0000";
   
-  
+  let x = 0;
+  let y = 0;
+  let index = 200;
 
+  while (index > 0) {
+
+    for (let i = 0; i < index; i += 20) {
+      context.fillRect(x,y,20,20);
+      x += 20;
+    }
+    index -= 20;
+    for (let i = 0; i < index; i += 20) {
+      context.fillRect(x,y,20,20);
+      y += 20;
+    }
+    index -= 20;
+    for (let i = 0; i < index; i += 20) {
+      context.fillRect(x,y,20,20);
+      x -= 20;
+    }
+    index -= 20;
+    for (let i = 0; i < index; i += 20) {
+      context.fillRect(x,y,20,20);
+      y -= 20;
+    }
+    index -= 20;
+    console.log("lets fucking go dude")
+  }
 }
